72b60195a326d337ce03f932eecbb22a0cfbdecc
EM.Nallar <miguel95.mn@gmail.com>
2024-07-13 18:23:18 -0300

    Concluindo com o projeto

--- MyContactApp/Crud.cs
+++ MyContactApp/Crud.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,7 +49,7 @@ namespace MyContactApp
         public void FindContact(string name)
         {
             var contact = contacts.FirstOrDefault(m => m.Name == name);
-            if(contact == null)
+            if (contact == null)
             {
                 Console.WriteLine("Contacto não encontrado");
             }
@@ -71,5 +73,156 @@ namespace MyContactApp
                 Console.WriteLine("Edição feita com sucesso");
             }
         }
+
+        public void SaveContact()
+        {
+            using (StreamWriter file = new StreamWriter("Contact.txt"))
+            {
+                if (File.Exists("Contact.txt"))
+                {
+                    string opcao = "y";
+                    Console.WriteLine("Deseja sobrescrivir o arquivo?: (y/n) ");
+                    opcao = Console.ReadLine();
+
+                    if (opcao.Equals("y"))
+                    {
+                        foreach (var contact in contacts)
+                        {
+                            file.WriteLine($"{contact.Name}, {contact.Description}");
+                        }
+                        Console.WriteLine("Arquivo salvo");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Arquivo não modificado");
+                    }
+                }
+            }
+        }
+        public void LoadContact()
+        {
+            if (File.Exists("Contact.txt"))
+            {
+                using (StreamReader file = new StreamReader("Contact.txt"))
+                {
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        var data = line.Split(",");
+                        var contact = new Contact()
+                        {
+                            Name = data[0],
+                            Description = data[1]
+
+                        };
+                        contacts.Add(contact);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Ordenar de forma asc e desc
+        /// </summary>
+        /// <param name="order">caso for null, return asc</param>   
+        public void OrderContact(string? order)
+        {
+            if (order == null || order.Equals("name", StringComparison.OrdinalIgnoreCase))
+            {
+                if (order.Equals("desc", StringComparison.OrdinalIgnoreCase))
+                {
+                    contacts = contacts.OrderByDescending(c => c.Name).ToList();
+
+                }
+                else
+                {
+                    contacts = contacts.OrderBy(c => c.Name).ToList();
+                }
+
+            }
+            else if (order.Equals("description", StringComparison.OrdinalIgnoreCase))
+            {
+                if (order.Equals("desc", StringComparison.OrdinalIgnoreCase))
+                {
+                    contacts = contacts.OrderByDescending(c => c.Description).ToList();
+                }
+                else
+                {
+                    contacts = contacts.OrderBy(c => c.Description).ToList();
+                }
+            }
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+        public void FilterContact(string filterBy, string filterValue)
+        {
+            IEnumerable<Contact> filteredContact = contacts;
+
+            if (filterBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+            {
+                filteredContact = contacts.Where(c => c.Name.Contains(filterValue, StringComparison.OrdinalIgnoreCase));
+            }
+            else if (filterBy.Equals("Description", StringComparison.OrdinalIgnoreCase))
+            {
+                filteredContact = contacts.Where(c => c.Description.Contains(filterValue, StringComparison.OrdinalIgnoreCase));
+            }
+            else
+            {
+                Console.WriteLine("Valor não esperado");
+                return;
+            }
+            foreach (var contact in filteredContact)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+        public void ImportContact()
+        {
+            if (!File.Exists("contact.csv"))
+            {
+                Console.WriteLine("Arquivo não encontrado");
+            }
+
+            using (var reader = new StreamReader("Contact.csv"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+
+                    if (values.Length > 0)
+                    {
+                        var contact = new Contact()
+                        {
+                            Name = values[0],
+                            Description = values[1]
+                        };
+                        contacts.Add(contact);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Formato invalido");
+                    }
+                    
+                }
+            }
+
+        }
+
+        public void ExportContact()
+        {
+            using (var writer = new StreamWriter("Contact.csv"))
+            {
+                foreach (var contact in contacts)
+                {
+                    writer.WriteLine($"{contact.Name},{contact.Description}");
+                }
+            }
+        }
     }
+
 }
--- MyContactApp/Program.cs
+++ MyContactApp/Program.cs
@@ -10,15 +10,15 @@ Crud crud = new Crud();
 
 while (options != 12)
 {
-    Console.WriteLine("1. Agregar Contacto\r\n" + //Feito
-                      "2. Eliminar Contacto\r\n" + //Feito
-                      "3. Buscar Contacto\r\n" +   //Feito
-                      "4. Listar Contactos\r\n" + //Feito
-                      "5. Editar Contacto\r\n" + //Feito
-                      "6. Guardar Contactos\r\n" +
-                      "7. Cargar Contactos\r\n" +
-                      "8. Ordenar Contactos\r\n" +
-                      "9. Filtrar Contactos\r\n" +
+    Console.WriteLine("1. Agregar Contacto\r\n" +
+                      "2. Eliminar Contacto\r\n" + 
+                      "3. Buscar Contacto\r\n" +   
+                      "4. Listar Contactos\r\n" + 
+                      "5. Editar Contacto\r\n" + 
+                      "6. Guardar Contactos\r\n" + 
+                      "7. Cargar Contactos\r\n" + 
+                      "8. Ordenar Contactos\r\n" + 
+                      "9. Filtrar Contactos\r\n" + 
                       "10. Importar Contactos\r\n" +
                       "11. Exportar Contactos\r\n" +
                       "12. Salir");
@@ -55,7 +55,38 @@ while (options != 12)
             name = Console.ReadLine();
             Console.WriteLine("Inserir Descrição: ");
             description = Console.ReadLine();
+
             crud.EditContact(oldName, name, description);
+
+            break;
+        case 6:
+            crud.SaveContact();
+            break;
+        case 7:
+            crud.LoadContact();
+            break;
+        case 8:
+            Console.WriteLine("Tipo de ordem: (asc/desc)");
+            string option = Console.ReadLine();
+            crud.OrderContact(option);
+            break;
+
+        case 9:
+            Console.WriteLine("Inserir o valor do filtro: (name/description)");
+            string filterBy = Console.ReadLine();
+            Console.WriteLine("Inserir o valor a ser pesquisado: ");
+            string filterContact = Console.ReadLine();
+
+            crud.FilterContact(filterBy, filterContact);
+            break;
+
+        case 10:
+
+            crud.ImportContact();
+            break;
+
+        case 11:
+            crud.ExportContact();
             break;
         case 12:
             break;
